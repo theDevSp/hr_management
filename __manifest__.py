4a77ac718b0f2b369f72485cddb498ad5c95b0eb
Codev022 <117266281+Codev022@users.noreply.github.com>
2022-12-14 15:32:19 +0100

    Version 14_12_2022_15h30
    Version 14_12_2022_15h30 :  Access Rules HR

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hr_management",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'name': "Human Ressources Management",
+    'summary': "The Human Ressources Management",
+    'description': "Description Of Human Ressources Management",
+    #'author': "My Company",
+    #'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '0.1',
+    "license": 'LGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'mail'],
 
     # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
+    'data': [         
         'views/views.xml',
         'views/templates.xml',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/employee_model/employee_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        #'demo/demo.xml',
     ],
+
+    'application': True ,
+    'installable': True ,
+    'auto_install': False,
+    'sequence': 2,
+    
 }
--- employee_model/employee.py
+++ employee_model/employee.py
@@ -1,18 +1,19 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from datetime import date
 
 class hr_employee(models.Model):
     _name = "hr.employee.new"
     _description = "Employee"
-    _order = 'name'
-   # _inherit = ['hr.employee']
+    #_order = 'name'
+    #_inherit = ['hr.employee']
     #_inherit = ['hr.employee', 'mail.thread', 'mail.activity.mixin', 'resource.mixin', 'avatar.mixin']
     #_mail_post_access = 'read'
 
     cin = fields.Char('CIN', required=True)
     is_salary = fields.Boolean("Is Employee?")
     ### employee_type = fields.Selection([("employee1","Salarié"),("employee2","Ouvrier")],string=u"Type d'employé",default="employee1")
-    title_id = fields.Many2one("res.partner.title","Titre")
-    bank = fields.Many2one("bank","Banque")
+    #title_id = fields.Many2one("res.partner.title","Titre")
+    #bank = fields.Many2one("bank","Banque")
     ville_bank = fields.Char(u"Ville")
     bank_agence = fields.Char(u"Agence")
 
@@ -36,10 +37,12 @@ class hr_employee(models.Model):
     motif_enbauche  = fields.Selection([("1","Satisfaire un besoin"),("2","Remplacement"),("3","Autre")],u"Motif d'embauche")
     obs_embauche  = fields.Char(u"Observation")
     employee_age = fields.Integer(u"Âge",compute="_compute_age")
-    ###date_naissance = fields.Date(u"Date Naissance")
-    ###lieu_naissance = fields.Char(u"Lieu Naissance")
+    
+    date_naissance = fields.Date(u"Date Naissance")
+    lieu_naissance = fields.Char(u"Lieu Naissance")
+    
     montant_cimr = fields.Float(u"Montant Cotisation CIMR")
-    nbr_jour_ferie = fields.Float(u"Nombre de Jours Fériés",compute="_compute_jf")
+   # nbr_jour_ferie = fields.Float(u"Nombre de Jours Fériés",compute="_compute_jf")
    # date_start = fields.Date(related="contract_id.date_start",string='Date Debut')
    # date_end = fields.Date(related="contract_id.date_end",string='Date Fin')
     #remaining_days = fields.Char(compute="_compute_remaining_days",string='Jours Restant')
@@ -58,3 +61,20 @@ class hr_employee(models.Model):
     #profile_paie = fields.Many2one(related="contract_id.function.function_id",string='Profile de Paie')
     #chantier_affect = fields.One2many("hr.employee.affectation.chantier","employee_id",string="Affectation Chantier")
     #company_id = fields.Many2one('res.company', 'Company', required=True,default=1)
+
+
+    #@api.multi
+    def _compute_age(self):
+        for employee in self:
+            if employee.date_naissance:
+                today = date.today() 
+                birthDate = fields.Date.from_string(employee.date_naissance)
+                age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
+                employee.employee_age = age       
+
+    #@api.multi
+    #def _compute_jf(self):  
+    #    for employee in self:  
+    #        if employee:
+    #            attributions = self.env['hr.holidays'].search([('employee_id','=',employee.id),('holiday_status_id','=',9)]) 
+    #            employee.nbr_jour_ferie = sum([line.number_of_days_temp for line in attributions])      
